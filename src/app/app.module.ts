92478079e13afe4040b9c2d1b7b0eaba77cc25f3
kelly kiiru <kelly.kiiru@student.moringaschool.com>
2022-04-07 01:29:42 +0300

    hard code interpolation component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,11 +4,13 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { MyFirstCustomComponentComponent } from './my-first-custom-component/my-first-custom-component.component';
+import { InterpolationComponent} from './interpolation-practise/interpolation'
 
 @NgModule({
   declarations: [
     AppComponent,
-    MyFirstCustomComponentComponent
+    MyFirstCustomComponentComponent,
+    InterpolationComponent
   ],
   imports: [
     BrowserModule,
