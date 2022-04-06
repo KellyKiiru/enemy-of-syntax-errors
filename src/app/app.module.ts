eafc2dfdadd149107f6737fab019a50e1618f793
kelly kiiru <kelly.kiiru@student.moringaschool.com>
2022-04-06 23:21:19 +0300

    add first angular component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,12 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { MyFirstCustomComponentComponent } from './my-first-custom-component/my-first-custom-component.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    MyFirstCustomComponentComponent
   ],
   imports: [
     BrowserModule,
---
+++ src/app/my-first-custom-component/my-first-custom-component.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-my-first-custom-component',
+  templateUrl: './my-first-custom-component.component.html',
+  styleUrls: ['./my-first-custom-component.component.css']
+})
+export class MyFirstCustomComponentComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
